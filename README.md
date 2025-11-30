91e56434ceeba30c9c7fed0620132951de7a3a80
Your Full Name <patrickmathiasorlik@gmail.com>
2025-11-30 11:14:31 +0100

    -Made the program work for N amount of dice and N amount of Rolls -Made the program take a user specified input instead of a hardcoded values -Added a readme.

---
+++ README.md
@@ -0,0 +1,35 @@
+A simple C program that simulates a user specified amount of Dice being Rolled for a specified amount of rolls. It prints out the distribution of the sums of the dicerolls.
+it should be able to handle most invalid user inputs but it is not completely safe.
+
+This program was made for my C programming Classes.
+
+
+
+
+usage example:
+how many Dice do you want? Max is 100 Dice
+3 (user input)
+How many rolls do you want? Max 50000
+100 (user input)
+3.0 
+4.1 
+5.0 
+6.4 
+7.7 
+8.13 
+9.17
+10.12
+11.8
+12.14
+13.11
+14.3
+15.4
+16.5
+17.1
+18.0
+
+
+
+
+
+Made By Patrick Orlik
--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define MAX_DICE 100
+#define MAX_ROLLS 50000
 
 unsigned int DiceSums[50000];
-unsigned int SumDistribution[11];
-unsigned int DiceAmount = 2;
-unsigned int RollAmount = 50000;
+unsigned int SumDistribution[601];
+unsigned int DiceAmount;
+unsigned int RollAmount;
 
+/**
+ * @brief a simple function that simulates X amount of Dice rolls with X amount
+ * of dice
+ *
+ * @param DiceAmount  = amount of Dice wanted
+ * @param RollAmount =  amount of rolls wanted
+ */
 void DiceRoll(int DiceAmount, int RollAmount) {
   for (int i = 0; i < RollAmount; i++) {
     int DiceSum = 0;
@@ -16,55 +25,46 @@ void DiceRoll(int DiceAmount, int RollAmount) {
     DiceSums[i] = DiceSum;
   }
 };
+/**
+ * @brief a function that checks the sum distribution of sums of dice rolls
+ * stored in an array works for N amount of Dice and rolls
+ *
+ * @param arr[] = Array storing sums
+ * @param size of the array storing sums =Arraysize
+ */
 void CheckSumDistribution(unsigned int arr[], int Arraysize) {
   for (int i = 0; i < Arraysize; i++) {
-    switch (arr[i]) {
-    case 2:
-      SumDistribution[0]++;
-      break;
-    case 3:
-      SumDistribution[1]++;
-      break;
-    case 4:
-      SumDistribution[2]++;
-      break;
-    case 5:
-      SumDistribution[3]++;
-      break;
-    case 6:
-      SumDistribution[4]++;
-      break;
-    case 7:
-      SumDistribution[5]++;
-      break;
-    case 8:
-      SumDistribution[6]++;
-      break;
-    case 9:
-      SumDistribution[7]++;
-      break;
-    case 10:
-      SumDistribution[8]++;
-      break;
-    case 11:
-      SumDistribution[9]++;
-      break;
-    case 12:
-      SumDistribution[10]++;
-      break;
-    }
+    SumDistribution[arr[i]]++;
   }
 }
-
-void PrintArray(int arr[], int ArraySize) {
-  for (int i = 0; i <= ArraySize; i++) {
-    fprintf(stdout, "%d. %d \n", i + 2, arr[i]);
+/**
+ * @brief Prints out a specified array storing sums of dicethrows
+ *
+ * @param the array that should be printed
+ * @param size of the array that should be printed
+ * @param amount of dice being rolled.
+ */
+void PrintArray(unsigned int arr[], int ArraySize, unsigned int DiceAmount) {
+  for (int i = DiceAmount; i <= (DiceAmount * 6); i++) {
+    fprintf(stdout, "%d. %d \n", i, arr[i]);
   }
 }
 
 int main() {
+
   srand(time(NULL));
-  DiceRoll(DiceAmount, RollAmount);
-  CheckSumDistribution(DiceSums, RollAmount);
-  PrintArray(SumDistribution, 10);
+  puts("how many Dice do you want? Max is 100 Dice");
+  fscanf(stdin, "%d", &DiceAmount);
+  if (DiceAmount > 0 && DiceAmount <= MAX_DICE) {
+    puts("How many rolls do you want? Max 50000");
+    fscanf(stdin, "%d", &RollAmount);
+    if (RollAmount > 0 && RollAmount <= 50000) {
+      DiceRoll(DiceAmount, RollAmount);
+      CheckSumDistribution(DiceSums, RollAmount);
+      PrintArray(SumDistribution, _countof(SumDistribution), DiceAmount);
+    } else
+      fprintf(stderr, "%s", "Error:Invalid user input");
+
+  } else
+    fprintf(stderr, "%s", "Error:Invalid user input");
 }
